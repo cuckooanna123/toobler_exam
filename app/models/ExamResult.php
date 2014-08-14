543e82d61a1e33a34496ada84c596a34bc5b9d30
Ans Maria Thomas <ans@toobler.com>
2014-08-14 18:23:57 +0530

    finish+timer

---
+++ app/models/ExamResult.php
@@ -0,0 +1,14 @@
+<?php 
+class ExamResult extends Eloquent{
+
+	
+	public $timestamps = false;
+	/**
+	 * The database table used by the model.
+	 * @var string
+	 */
+	protected $table = 'exam_results';
+
+
+	
+}
