dea26179add7235c1cbb312753dc746b3ebde33a
Ans Maria Thomas <ans@toobler.com>
2014-08-14 10:27:33 +0530

    exam data model

---
+++ app/models/ExamData.php
@@ -0,0 +1,14 @@
+<?php 
+class ExamData extends Eloquent{
+
+	
+	public $timestamps = false;
+	/**
+	 * The database table used by the model.
+	 * @var string
+	 */
+	protected $table = 'exam_data';
+
+
+	
+}
